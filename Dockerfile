923828f01c7304278271f84520fded8f392bd20c
Tulir Asokan <tulir@maunium.net>
2018-09-20 01:14:14 +0300

    Improve bot

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY . /go/src/maubot.xyz/sed
 RUN go build -buildmode=plugin -o /maubot-plugins/sed.mbp maubot.xyz/sed
 
 FROM scratch
-COPY --from=builder /maubot-plugins/sed.mbp /maubot-plugins/sed.mbp
 VOLUME /output
-CMD ["cp", "/maubot-plugins/sed.mbp", "/output/sed.mbp"]
+COPY --from=builder /maubot-plugins/sed.mbp /output/sed.mbp
+# CMD ["cp", "/maubot-plugins/sed.mbp", "/output/sed.mbp"]
--- sed.go
+++ sed.go
@@ -20,17 +20,55 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"sync"
 
 	"maubot.xyz"
+	"maunium.net/go/gomatrix"
 )
 
 type Sed struct {
-	client maubot.MatrixClient
-	prevEventMap map[string]map[string]string
+	client         maubot.MatrixClient
+	log            maubot.Logger
+	prevEventLock  sync.RWMutex
+	prevEventMap   map[string]map[string]string
+	prevEventQueue map[string]*EventQueue
 }
 
+func NewEventQueue() *EventQueue {
+	return &EventQueue{
+		List: make([]*maubot.Event, 10),
+		Ptr:  0,
+	}
+}
+
+type EventQueue struct {
+	List []*maubot.Event
+	Ptr  int
+}
+
+func (eq *EventQueue) Add(evt *maubot.Event) {
+	eq.List[eq.Ptr] = evt
+	eq.Ptr = (eq.Ptr + 1) % len(eq.List)
+}
+
+const (
+	CommandShortSed = "net.maunium.sed.short"
+	CommandLongSed  = "net.maunium.sed.long"
+)
+
 func (bot *Sed) Start() {
-	bot.client.AddEventHandler("m.room.message", bot.MessageHandler)
+	bot.client.SetCommandSpec(&maubot.CommandSpec{
+		PassiveCommands: []maubot.PassiveCommand{{
+			Name:         CommandShortSed,
+			Matches:      `^s([#/])(.*?[^\\]?)[#/](.*?[^\\]?)(?:[#/]([gi]+)?)?$`,
+			MatchAgainst: maubot.MatchAgainstBody,
+		}, {
+			Name:         CommandLongSed,
+			Matches:      `sed s(.)(.*?[^\\]?)\1(.*?[^\\]?)\1([gi]+)?`,
+			MatchAgainst: maubot.MatchAgainstBody,
+		}},
+	})
+	bot.client.AddEventHandler(gomatrix.EventMessage, bot.MessageHandler)
 }
 
 func (bot *Sed) Stop() {}
@@ -41,18 +79,24 @@ type SedStatement struct {
 	Global  bool
 }
 
+var shortSedRegex = regexp.MustCompile(`^s[#/](.*?[^\\]?)[#/](.*?[^\\]?)(?:[#/]([gi]+)?)?$`)
+
 func (bot *Sed) ParseEvent(evt *maubot.Event) (*SedStatement, error) {
-	finder := bot.findShortStatement(evt)
-	if finder != nil {
-		sed, err := bot.compileStatement(evt, finder)
-		if err != nil {
-			return nil, err
-		} else if sed != nil {
-			return sed, nil
-		}
+	sed, err := bot.compilePassiveStatement(evt)
+	if err != nil {
+		return nil, err
+	} else if sed != nil {
+		return sed, nil
 	}
 
-	finder = bot.findFullStatement(evt)
+	/*sed, err = bot.compileStatement(evt, shortSedRegex)
+	if err != nil {
+		return nil, err
+	} else if sed != nil {
+		return sed, nil
+	}*/
+
+	finder := bot.findFullStatement(evt)
 	if finder != nil {
 		sed, err := bot.compileStatement(evt, finder)
 		if err != nil {
@@ -75,18 +119,40 @@ func (bot *Sed) findFullStatement(evt *maubot.Event) *regexp.Regexp {
 	return regexFinder
 }
 
-func (bot *Sed) findShortStatement(evt *maubot.Event) *regexp.Regexp {
-	if len(evt.Content.Body) < 4 || evt.Content.Body[0] != 's' {
-		return nil
+func (bot *Sed) compilePassiveStatement(evt *maubot.Event) (*SedStatement, error) {
+	if evt.Unsigned.PassiveCommand == nil {
+		return nil, nil
+	}
+	var matchedCommand *gomatrix.MatchedPassiveCommand
+	var ok bool
+	if matchedCommand, ok = evt.Unsigned.PassiveCommand[CommandShortSed]; !ok {
+		if matchedCommand, ok = evt.Unsigned.PassiveCommand[CommandLongSed]; !ok {
+			return nil, nil
+		}
+	}
+	captured := matchedCommand.Captured
+	if len(captured) == 0 || len(captured[0]) != 6 {
+		return nil, nil
+	}
+	match := captured[0]
+
+	regex, err := regexp.Compile(match[3])
+	if err != nil {
+		return nil, fmt.Errorf("failed to compile regex: %v", err)
 	}
 
-	separator := evt.Content.Body[1]
-	regexFinder, _ := regexp.Compile(fmt.Sprintf(`^s%[1]s(.*?[^\\]?)%[1]s(.*?[^\\]?)%[1]s([gi]+)?$`, regexp.QuoteMeta(string(separator))))
-	return regexFinder
+	flags := match[5]
+
+	return &SedStatement{
+		Find:    regex,
+		Replace: match[4],
+		Global:  strings.ContainsRune(flags, 'g'),
+	}, nil
 }
 
 func (bot *Sed) compileStatement(evt *maubot.Event, finder *regexp.Regexp) (*SedStatement, error) {
 	match := finder.FindStringSubmatch(evt.Content.Body)
+	bot.log.Debugln(evt.Content.Body, "---", finder, "---", match)
 	if len(match) != 4 {
 		return nil, nil
 	}
@@ -121,15 +187,26 @@ func (sed *SedStatement) Exec(body string) string {
 }
 
 func (bot *Sed) RegisterPrevEvent(evt *maubot.Event) {
+	bot.prevEventLock.Lock()
 	roomMap, ok := bot.prevEventMap[evt.RoomID]
 	if !ok {
 		roomMap = make(map[string]string)
 		bot.prevEventMap[evt.RoomID] = roomMap
 	}
 	roomMap[evt.Sender] = evt.ID
+
+	roomPrevEventQueue, ok := bot.prevEventQueue[evt.RoomID]
+	if !ok {
+		roomPrevEventQueue = NewEventQueue()
+		bot.prevEventQueue[evt.RoomID] = roomPrevEventQueue
+	}
+	roomPrevEventQueue.Add(evt)
+
+	bot.prevEventLock.Unlock()
 }
 
 func (bot *Sed) GetPrevEvent(roomID, userID string) *maubot.Event {
+	bot.prevEventLock.RLock()
 	roomMap, ok := bot.prevEventMap[roomID]
 	if !ok {
 		return nil
@@ -139,8 +216,37 @@ func (bot *Sed) GetPrevEvent(roomID, userID string) *maubot.Event {
 	if !ok {
 		return nil
 	}
+	bot.prevEventLock.RUnlock()
 
 	return bot.client.GetEvent(roomID, eventID)
+}
+
+func (bot *Sed) TryReplaceEvent(sed *SedStatement, evt, origEvt *maubot.Event) bool {
+	replaced := sed.Exec(origEvt.Content.Body)
+	if replaced == origEvt.Content.Body {
+		return false
+	}
+	origEvt.Reply(replaced)
+	return true
+}
+
+func (bot *Sed) TryReplaceRecentEvent(sed *SedStatement, evt *maubot.Event) bool {
+	bot.prevEventLock.RLock()
+	roomPrevEventQueue, ok := bot.prevEventQueue[evt.RoomID]
+	if !ok {
+		return false
+	}
+	bot.prevEventLock.RUnlock()
+
+	origPtr := roomPrevEventQueue.Ptr
+	listLen := len(roomPrevEventQueue.List)
+	for i := listLen; i > 0; i-- {
+		origEvt := roomPrevEventQueue.List[(i+origPtr)%listLen]
+		if origEvt != nil && bot.TryReplaceEvent(sed, evt, origEvt) {
+			return true
+		}
+	}
+	return false
 }
 
 func (bot *Sed) MessageHandler(evt *maubot.Event) maubot.EventHandlerResult {
@@ -155,26 +261,28 @@ func (bot *Sed) MessageHandler(evt *maubot.Event) maubot.EventHandlerResult {
 	}
 
 	var origEvt *maubot.Event
-	if len(evt.Content.RelatesTo.InReplyTo.EventID) > 0 {
-		origEvt = bot.client.GetEvent(evt.RoomID, evt.Content.RelatesTo.InReplyTo.EventID)
+	if len(evt.Content.GetReplyTo()) > 0 {
+		origEvt = bot.client.GetEvent(evt.RoomID, evt.Content.GetReplyTo())
 	} else {
 		origEvt = bot.GetPrevEvent(evt.RoomID, evt.Sender)
 	}
-	if origEvt == nil {
-		return maubot.Continue
-	}
-
 	evt.MarkRead()
-	replaced := sed.Exec(origEvt.Content.Body)
-	origEvt.Reply(replaced)
+
+	if origEvt == nil || !bot.TryReplaceEvent(sed, evt, origEvt) {
+		if !bot.TryReplaceRecentEvent(sed, evt) {
+			return maubot.Continue
+		}
+	}
 	return maubot.StopEventPropagation
 }
 
 var Plugin = maubot.PluginCreator{
-	Create: func(client maubot.MatrixClient) maubot.Plugin {
+	Create: func(client maubot.MatrixClient, logger maubot.Logger) maubot.Plugin {
 		return &Sed{
-			client: client,
-			prevEventMap: make(map[string]map[string]string),
+			client:         client,
+			log:            logger,
+			prevEventMap:   make(map[string]map[string]string),
+			prevEventQueue: make(map[string]*EventQueue),
 		}
 	},
 	Name:    "maubot.xyz/sed",
