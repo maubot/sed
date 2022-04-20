f66d3c783ded688d2a782304cbbb01f43cfebda2
Tulir Asokan <tulir@maunium.net>
2022-04-20 23:19:58 +0300

    Bump version to 1.1.0

--- sed.py
+++ sed.py
@@ -1,5 +1,5 @@
 # sed - A maubot plugin to do sed-like replacements.
-# Copyright (C) 2020 Tulir Asokan
+# Copyright (C) 2022 Tulir Asokan
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
